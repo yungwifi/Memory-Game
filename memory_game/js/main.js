49fcf97f595ce70d39eae36ae02fd6676dcea4ae
yungwifi <smerryman@me.com>
2018-03-07 15:09:06 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "Queen";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "King";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);

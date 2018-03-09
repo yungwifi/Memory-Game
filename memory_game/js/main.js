56972342a7f17fe321db3f517b51995f580d1945
yungwifi <smerryman@me.com>
2018-03-08 20:22:00 -0500

    created game logic v3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,41 @@
-var cards = ['queen', 'king', 'queen', 'king'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay = []
 
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You've found a match!");
-
-	] else {
-		console.log("Sorry, try again.");
+		alert("You've found a match!");
+    }
+	 else {
+		alert("Sorry, try again.");
 	}
 }
 var flipCard = function(cardId){
-console.log("User Flipped" + ' ' + cards[cardId]);
-cardsInPlay.push(cards[cardId])
+console.log("User Flipped" + ' ' + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank)
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 }
 
 flipCard(0);
